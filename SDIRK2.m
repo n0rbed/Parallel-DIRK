e9f56057bc84be5058b2354ef9879c3b31f18a7d
Yassin ElBedwihy <58118886+n0rbed@users.noreply.github.com>
2024-12-05 19:01:44 +0200

    Add files via upload

--- SDIRK2.m
+++ SDIRK2.m
@@ -1,26 +1,34 @@
-function [y_prediction] = SDIRK2_2(f, t_0, y_0, h, n_steps)
-    y_prediction = [];
+function [y_prediction, t] = SDIRK2(f, t_0, y_0, h, n_steps)
+    y_prediction = zeros(length(y_0), n_steps + 1);
+    y_prediction(:, 1) = y_0;
+    t = zeros(length(t_0), n_steps+1);
+    t(:, 1) = t_0;
+   
     y_n = y_0;
     t_n = t_0;
-    syms k;
-    for i = 1:n_steps
-        % Stage 1
-        c1 = 1-sqrt(2)/2; 
-        a11=sqrt(2)/2; a12 = 0;
     
-        % find k1 using a numerical solver
-        k1 = vpasolve(k == f(t_n+c1*h, y_n+h*a11*k))
-        
-        % Stage 2
-        c2 = 1;
-        a21 = sqrt(2)/2; a22 = 1-sqrt(2)/2;
-        
-        k2 = vpasolve(k == f(t_n+c2*h,y_n+h*(a21*k1+a22*k)))
-        
-        % Compute the next step (y_next)
-        b1 = sqrt(2)/2; b2 = 1-sqrt(2)/2;
-        y_prediction(i) = y_n + h*(b1*k1+b2*k2);
-        y_n = y_prediction(i);
+    %coefficients
+    c1 = 1 - sqrt(2)/2; c2 = 1;
+
+    a11 = sqrt(2)/2;  a12 = 0;
+    a21 = sqrt(2)/2;  a22 = 1 - sqrt(2)/2;
+
+    b1 = sqrt(2)/2; b2 = 1 - sqrt(2)/2;
+
+    for i = 2:(n_steps + 1)
+        % Stage 1: Solve for k1
+        g1 = @(k) k - f(t_n + c1*h, y_n + h*a11*k); 
+        k1 = fsolve(g1, y_n, optimset('Display','off')); % Use y_n as the initial guess for k1
+
+        % Stage 2: Solve for k2
+        g2 = @(k) k - f(t_n + c2*h, y_n + h*(a21*k1 + a22*k)); 
+        k2 = fsolve(g2, y_n, optimset('Display','off')); % Use y_n as the initial guess for k2
+
+        % Compute the next step
+        y_n = y_n + h*(b1*k1 + b2*k2);
+        y_prediction(:, i) = y_n; 
         t_n = t_n + h;
+        t(:, i) = t_n;
     end
 end
+
